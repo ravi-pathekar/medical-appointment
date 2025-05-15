fc5d76f2cecb84aea037cac94c03f411434fd91e
ravi-pathekar <ravipathekar99@gmail.com>
2025-05-15 08:55:11 +0530

    Added svix verification in clerk webhook route to handle webhook properly

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,15 +1,56 @@
 import axios from "axios";
-import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { headers } from "next/headers";
 import { createClerkClient } from "@clerk/backend";
-import { NextRequest, NextResponse } from "next/server";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import { Webhook } from "svix";
 
-export async function POST(req: NextRequest) {
+export async function POST(req: Request) {
   try {
-    const clerkClient = createClerkClient({
-      secretKey: process.env.CLERK_SECRET_KEY,
-      publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    });
-    const evt = await verifyWebhook(req);
+    const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SIGNING_SECRET;
+
+    if (!WEBHOOK_SECRET) {
+      throw new Error(
+        "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+      );
+    }
+
+    // Get the headers
+    const headerPayload = headers();
+    const svix_id = headerPayload.get("svix-id");
+    const svix_timestamp = headerPayload.get("svix-timestamp");
+    const svix_signature = headerPayload.get("svix-signature");
+
+    // If there are no headers return Error
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+      return new NextResponse("Error: No svix headers", {
+        status: 400,
+      });
+    }
+
+    // Get the body
+    const payload = await req.json();
+    const body = JSON.stringify(payload);
+
+    // Create a new Svix instance with your secret.
+    const wh = new Webhook(WEBHOOK_SECRET);
+
+    let evt: WebhookEvent;
+
+    // Verify the payload with the headers
+    try {
+      evt = wh.verify(body, {
+        "svix-id": svix_id,
+        "svix-timestamp": svix_timestamp,
+        "svix-signature": svix_signature,
+      }) as WebhookEvent;
+    } catch (err) {
+      console.error("Something went wrong while verifying webhook:", err);
+      return new NextResponse("Error occured", {
+        status: 400,
+      });
+    }
+
     const {
       data: { id },
       type: eventType,
@@ -17,7 +58,12 @@ export async function POST(req: NextRequest) {
     
     console.log(`Webhook with Id ${id} and event type is ${eventType}`);
     console.log("Webhook event data", evt.data)
-    // Create user in the db
+
+    const clerkClient = createClerkClient({
+      secretKey: process.env.CLERK_SECRET_KEY,
+      publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    });
+
     if (eventType === "user.created") {
       const {
         id,
@@ -28,16 +74,15 @@ export async function POST(req: NextRequest) {
         phone_numbers: phoneNumbers,
       } = evt.data;
 
-
       const createdUser = await axios.post(
         "http://localhost:5000/api/users",
         {
-            email: emailAddresses[0].email_address,
-            firstName,
-            lastName,
-            photo,
-            phone: phoneNumbers[0].phone_number,
-            clerkAuthId: id,
+          email: emailAddresses[0].email_address,
+          firstName,
+          lastName,
+          photo,
+          phone: phoneNumbers[0].phone_number,
+          clerkAuthId: id,
         },
         {
           headers: {
@@ -59,12 +104,16 @@ export async function POST(req: NextRequest) {
 
     if (eventType === "user.updated" || eventType === "user.deleted") {
       const { id: userId } = evt.data;
-      const session = await clerkClient.sessions.createSession({ userId: userId as string });
+      const session = await clerkClient.sessions.createSession({
+        userId: userId as string,
+      });
 
-      token = await clerkClient.sessions.getToken(
-        session.id,
-        "medical-appointment-jwt-token"
-      );
+      if (process.env.NEXT_PUBLIC_CLERK_JWT_TEMPLATE) {
+        token = await clerkClient.sessions.getToken(
+          session.id,
+          process.env.NEXT_PUBLIC_CLERK_JWT_TEMPLATE
+        );
+      }
     }
 
     // Update user details in the db
@@ -99,9 +148,9 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ message: "OK", userDeleted: true });
     }
 
-    return new Response("Webhook received", { status: 200 });
+    return new NextResponse("Webhook received", { status: 200 });
   } catch (err) {
     console.error("Error verifying webhook:", err);
-    return new Response("Something went wrong in webhook", { status: 400 });
+    return new NextResponse("Something went wrong in webhook", { status: 400 });
   }
 }
